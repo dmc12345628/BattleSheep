d71a726c0b752d2db7e93933f33d0b7402b34545
Raphael D <raphael.dincau@hotmail.fr>
2018-03-05 16:04:54 +0100

    Last modifications

---
+++ Assets/SoloPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoloPlayer : MonoBehaviour {
+
+	public enum ButtonStates
+	{
+		//default states
+		Empty,
+		Sheep,
+		//clicked states
+		EmptyClicked,
+		SheepClicked
+	}
+
+
+	private ButtonStates[,] tableButtons;
+
+	// Use this for initialization
+	void Start () {
+		tableButtons = new ButtonStates[,]{
+			{ButtonStates.Sheep, ButtonStates.Empty, ButtonStates.Empty, ButtonStates.Empty, ButtonStates.Empty},
+			{ButtonStates.Sheep, ButtonStates.Empty, ButtonStates.Empty, ButtonStates.Empty, ButtonStates.Empty},
+			{ButtonStates.Sheep, ButtonStates.Empty, ButtonStates.Empty, ButtonStates.Empty, ButtonStates.Empty}
+		};
+
+
+
+		
+	}
+
+	public void Click(string msg){
+
+
+	}
+
+	/*public int[] Convert(string ) {
+
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/getTextBtn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class getTextBtn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void getBtnText()
+	{
+		string text = this.GetComponentInChildren<Text> ().text;
+		print (text);
+		//return text;
+	}
+}
