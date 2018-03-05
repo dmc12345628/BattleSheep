a2d13f3fd67ede3fc5d0ecba15400ea3ce1ed7bd
Jesus Daniel Medina Cruz <0315108270@miutt.edu.mx>
2018-03-05 16:57:03 +0100

    MainMenu was added

---
+++ Assets/Scripts/MainMenu/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	public Button jouer;
+
+	// Use this for initialization
+	void Start () {
+		if (jouer != null) {
+			jouer.onClick.AddListener (StartGame);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void StartGame() {
+		
+	}
+}
